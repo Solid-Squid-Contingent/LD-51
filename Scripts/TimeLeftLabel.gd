6fb3c13f7866791eb6c970130f7dbad34e0f08e5
Julian Berger <julian.berger@student.hpi.de>
2022-10-02 22:52:15 +0200

    New timer font

--- Scripts/TimeLeftLabel.gd
+++ Scripts/TimeLeftLabel.gd
@@ -1,15 +1,37 @@
-extends Label
+extends Node2D
 
 signal timeUp
 
 var timeLeft: int
 var minLength: int
 
+var sprites = []
+
+const zeroTexture = preload("res://Resources/Graphics/Misc/number_0.png")
+const oneTexture = preload("res://Resources/Graphics/Misc/number_1.png")
+
 func _ready():
 	setTime(16)
 
 func updateText():
-	text = Base.toBinaryString(timeLeft, minLength) +"s left"
+	for sprite in sprites:
+		sprite.queue_free()
+	sprites = []
+	
+	var text = Base.toBinaryString(timeLeft, minLength)
+	
+	var nextPosition = Vector2(0,0)
+	for digit in text:
+		var sprite = Sprite.new()
+		sprite.centered = false
+		if digit == "0":
+			sprite.texture = zeroTexture
+		else:
+			sprite.texture = oneTexture
+		sprite.position = nextPosition
+		nextPosition.x += sprite.texture.get_size().x
+		add_child(sprite)
+		sprites.append(sprite)
 	
 func setTime(time):
 	timeLeft = time
